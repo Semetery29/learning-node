dca97bc5332c268c121fa01ee2c9a5a1bb9193f9
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-11-24 04:11:46 +0600

    custom router added

--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -6,6 +6,36 @@ const port = 3000;
 app.use(express.json());
 app.use(express.text());
 
+//
+
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+
+  res.json({
+    success: true,
+    message: "Course is enrolled successfully",
+    data: course,
+  });
+});
+
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
 
